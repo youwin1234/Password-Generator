150fefb7f1a8960358d5f25b1acf42018e0c5180
youwin1234 <tebbytoad@gmail.com>
2020-04-08 00:13:35 -0400

    Password Generator Project
    Publishing my repository of my password generator built in Angular and using Bulma for styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()_-+=<>,.?/;:"[]{}|\'\\';
+
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+    
+    let generatedPassword = '';
+
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  useLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  useNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  
+  useSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value:string) {
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
 }
